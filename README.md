1f6cdb59fc1476689f8483e8c92a5d7f9f655ff1
Amireza <arfo.amireza@gmail.com>
2015-06-08 20:17:40 +0800

    Adding post request endpoint for save
    For now it is really basic and in health.rb file also
    need to move to its own file and also get polished and handled error

---
+++ README.md
@@ -0,0 +1,3 @@
+ To send jason post request 
+     curl -H "Content-Type: application/json" -X POST -d '{"date":"Am I nailed it ?????????"}' http://localhost:80/save
+ 
--- api/health.rb
+++ api/health.rb
@@ -5,9 +5,29 @@ class Health < Grape::API
   version 'v1', using: :header, vendor: 'iotcc' 
  
   resource :health do
-   desc "Checking API health"
+   #desc "Checking API health"
    get do
+     "testing health........."
      "Service is running"
+   end
+  end
+ 
+  resource :save do
+   #decs "saving data into the database"
+   
+   params do 
+     requires :date, type: String, desc: "Triggered date"
+   end
+
+   post do 
+       
+    begin 
+      result = "it is accepted"
+      valDate = params[:date]
+      File.open('date.txt', 'w') { |file| file.write(valDate.to_s) }
+    rescue ArgumentError => e 
+    end
+ 
    end
   end
 end
