e171ea36afb8af23522c855d6f3ef523f599b987
Amireza <arfo.amireza@gmail.com>
2015-07-07 20:02:17 +0800

    Added insert method to the save_temp class

--- lib/save_temp.rb
+++ lib/save_temp.rb
@@ -1,4 +1,12 @@
 
 class Save_temp
+   attr_accessor :data
+   
+   def initialize
+     @data = {}
+   end
 
+   def insert(temp_data)
+     @data = temp_data
+   end   
 end
--- test/save_temp_test.rb
+++ test/save_temp_test.rb
@@ -12,6 +12,18 @@ class TestSaveEndpoint < Minitest::Test
      begin
        Save_temp.new
      rescue 
+       raise 'Class is not existing'
      end
    end
+
+   def test_save_temp_class_initialize
+      data = {temp: 15, date: 'today'}
+    begin 
+      insert = Save_temp.new.insert(data)
+      assert_equal 15, insert[:temp], "Tempater is not match"
+    rescue Exception => e   
+      raise 'access problem to tempature'  
+      p e
+    end
+   end 
 end
