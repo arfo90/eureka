1177a89e057a469b1f56838ddc38f8a01af4dfc5
arfo90 <arfo.amireza@gmail.com>
2015-08-16 03:52:20 +0800

    Adding the api files back
    *refactoring

---
+++ lib/api/health.rb
@@ -0,0 +1,14 @@
+require 'grape'
+
+class Health < Grape::API
+  
+  version 'v1', using: :header, vendor: 'iotcc' 
+ 
+  resource :health do
+   #desc "Checking API health"
+   get do
+     "testing health........."
+     "Service is running"
+   end
+  end
+end
