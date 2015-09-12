36f96ac5f8b561c6903b73ffb3775b595af9747d
arfo90 <arfo.amireza@gmail.com>
2015-09-12 15:42:49 +0800

    Simple save method for DepotService
    This method calls storage to save api request data

--- test/service/depot_service_test.rb
+++ test/service/depot_service_test.rb
@@ -5,4 +5,8 @@ class TestDepotService <  TestBase
   def test_calss_exist
     depot = Storage::Service::DepotService.new
   end
+
+  def test_calss_has_save_method
+    assert Storage::Service::DepotService.method_defined? :save
+  end
 end
