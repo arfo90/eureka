49b60c1c6e868d7275d9b8740a03ed62e22009b3
Amireza Fatemi <arfo.amireza@gmail.com>
2018-03-24 22:53:53 +0100

    Add pass generator package

---
+++ pass-gerenrator/pass_generator.go
@@ -0,0 +1,3 @@
+package passGenerator
+
+func Generate() {}
---
+++ pass-gerenrator/pass_generator_test.go
@@ -0,0 +1,11 @@
+package passGenerator_test
+
+import (
+	"testing"
+
+	"passGenerator"
+)
+
+func generatorTest(t *testing.T) {
+	password := passGenerator.Generate()
+}
