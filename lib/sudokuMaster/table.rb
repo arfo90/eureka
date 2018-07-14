76713ee70c745e221bd09c71546d2dfe24103b23
Amireza Fatemi <arfo.amireza@gmail.com>
2018-07-14 12:47:31 +0200

    Create 9X9 grid based on input numbers

--- lib/sudokuMaster/table.rb
+++ lib/sudokuMaster/table.rb
@@ -1,4 +1,16 @@
 module Sudokumaster
   class Table
+    attr_reader :grid
+
+    def initialize(numbers)
+      validate(numbers)
+
+      @grid = numbers
+    end
+
+    def validate(numbers)
+      raise ArgumentError, 'Input value, has wrong number of row' unless numbers.size == 9
+      raise ArgumentError, 'Input value, has wrong number of col' unless numbers[0].size == 9
+    end
   end
 end
--- test/table_test.rb
+++ test/table_test.rb
@@ -1,9 +1,19 @@
 require_relative './base_test'
 
 class TestTable < TestBase
+  def setup
+    @nums = Array.new(9){Array.new(9)}
+  end
+
   def test_class_exists
     assert_nothing_raised "Class doesn't exists" do
-      Sudokumaster::Table.new
+      Sudokumaster::Table.new(@nums)
     end
+  end
+
+  def test_table_initilize_9X9_grid
+    grid = Sudokumaster::Table.new(@nums).grid
+    assert_equal(9, grid.size, "Grid doesn't have expected row")
+    assert_equal(9, grid[0].size, "Grid doesn't have expected col")
   end
 end
