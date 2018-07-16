1800f040a106d7c10fd82da79346f95759297100
Amireza Fatemi <arfo.amireza@gmail.com>
2018-07-17 01:00:59 +0200

    Some cleanup

--- lib/sudokuMaster/strategy/simple_player.rb
+++ lib/sudokuMaster/strategy/simple_player.rb
@@ -7,10 +7,7 @@ module Sudokumaster
         grid[0].size.times do |i|
           grid[0][0].size.times do |j|
             if grid[i][j] == '*' || grid[i][j] == 0
-              dead_nums = []
-              dead_nums << table.row(i)
-              dead_nums << table.col(j)
-              dead_nums << table.get_block_by_cell(i,j)
+              dead_nums = [] << table.row(i) << table.col(j) << dead_nums << table.get_block_by_cell(i,j)
               possible_num = Array(1..9) - dead_nums.flatten
               if possible_num.size == 1
                 table.set_cell(i, j, possible_num[0])
