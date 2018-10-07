62d734b7f807aeed7d504673ba8afcd5ed812f07
Amireza Fatemi <arfo.amireza@gmail.com>
2018-10-08 00:07:46 +0200

    Basic tf defenition, declaring provider

---
+++ 2kagro/infrastructure/var.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  description = "aws access key required from proper AMI user"
+  ddefault    = ""
+}
+
+variable "aws_secret_key" {
+  description = "aws secret key required from proper AMI user"
+  ddefault    = ""
+}
