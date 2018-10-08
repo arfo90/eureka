a9735c71674c75084864bff570607f5727e4a01b
Amireza Fatemi <arfo.amireza@gmail.com>
2018-10-08 02:31:04 +0200

    Add APIGetAway to definition

--- 2kagro/infrastructure/main.tf
+++ 2kagro/infrastructure/main.tf
@@ -3,3 +3,102 @@ provider "aws" {
   secret_key = "${var.aws_secret_key}"
   region     = "eu-west-1"
 }
+
+resource "aws_lambda_function" "2kagro-logger" {
+  function_name = "TwoKAgroLogger"
+
+  s3_bucket = "2kagro-lambdas-code"
+  s3_key    = "v1.0.0/logger.zip"
+
+  handler = "main"
+  runtime = "go1.x"
+
+  role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_2kagro-api_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_api_gateway_rest_api" "2kagro-api" {
+  name        = "TwoAgroAPI"
+  description = "2Kagro API Getaway"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.2kagro-api.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.2kagro-logger.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.2kagro-api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.2kagro-logger.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "2kagro-api" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  stage_name  = "test"
+}
+
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.2kagro-logger.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.2kagro-api.execution_arn}/*/*"
+}
--- 2kagro/infrastructure/var.tf
+++ 2kagro/infrastructure/var.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
   description = "aws access key required from proper AMI user"
-  ddefault    = ""
+  default    = ""
 }
 
 variable "aws_secret_key" {
   description = "aws secret key required from proper AMI user"
-  ddefault    = ""
+  default    = ""
 }
