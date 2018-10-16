7358cf755e6aec038c06630e83ecd8fbf1645541
Amireza Fatemi <arfo.amireza@gmail.com>
2018-10-16 10:52:54 +0200

    Tidying up infra

--- 2kagro/infrastructure/api_gateway.tf
+++ 2kagro/infrastructure/api_gateway.tf
@@ -1,1 +1,54 @@
+resource "aws_api_gateway_rest_api" "2kagro-api" {
+  name        = "TwoAgroAPI"
+  description = "2Kagro API Getaway"
+}
 
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.2kagro-api.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.2kagro-logger.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.2kagro-api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.2kagro-logger.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "2kagro-api" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.2kagro-api.id}"
+  stage_name  = "api"
+}
--- 2kagro/lambdas/logger/main.go
+++ 2kagro/lambdas/logger/main.go
@@ -5,31 +5,41 @@ import (
   "net/http"
   "encoding/json"
 
-  "github.com/arfo90/eureka/2kagro/lib/models"
+  _"github.com/arfo90/eureka/2kagro/lib/models"
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
 )
 
+type field struct {
+  Id        float64   `json:"id"`
+  LocLang   string    `json:"loclang"`
+  LocLat    string    `json:"loclat"`
+  Moister   float64   `json:"moister"`
+  Nitra     float64   `json:"nitra"`
+  Ph        float64   `json:"ph"`
+}
+
 func create(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
   if req.Headers["Content-Type"] != "application/json" {
     return clientError(http.StatusNotAcceptable)
   }
 
-  fd := new(models.Field)
+  fd := new(field)
   err := json.Unmarshal([]byte(req.Body), fd)
 
   if err != nil {
-    return clientError(http.StatusUnprocessableEntity)
+    //return clientError(http.StatusUnprocessableEntity)
+    return showerror(err)
   }
 
   if fd.Id == 0 {
     return clientError(http.StatusBadRequest)
   }
 
-
   return events.APIGatewayProxyResponse{
     StatusCode: 201,
-    Body: fmt.Sprintf("Processed request ID %f, following details mois:%f ph:%f nitra:%f",  fd.Id, fd.Moister, fd.Nitra, fd.Ph),
+    Body: fmt.Sprintf("Processed request ID %f, following details mois:%f ph:%f nitra:%f", fd.Id, fd.Moister),
   }, nil
 }
 
@@ -38,6 +48,13 @@ func clientError(status int) (events.APIGatewayProxyResponse, error) {
     StatusCode: status,
     Body:       http.StatusText(status),
   }, nil
+}
+
+func showerror(err error) (events.APIGatewayProxyResponse, error) {
+  return events.APIGatewayProxyResponse{
+    StatusCode: 400,
+    Body:       "error",
+  }, err
 }
 
 func main(){
